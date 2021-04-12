702a60f514cb1415fe7e18611def90f6e32b5b4f
Mikael Sahleström <mikael.sahlestrom@gmail.com>
2021-04-12 11:02:14 +0200

    add recepie presentation and js module

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 import mat from './matlista.js'
 
-const a = Object.keys(mat).length;
-document.getElementById("recepie-count").innerHTML = a;
+function räknaRecept(){
+    const antalRecept = Object.keys(mat).length;
+    document.getElementById("recepie-count").innerHTML = antalRecept;
+}
+
+function recept(){
+    const recepieList = document.getElementById("recept")
+    
+    Object.keys(mat).forEach(
+        (mat) => {
+            const newRecepie = document.createElement("ol")
+            let text = document.createTextNode(mat)
+            newRecepie.appendChild(text)        
+            recepieList.appendChild(newRecepie)
+       }
+        )
+}
+
+
+// content loader
+document.getElementById("app").onload = () => {
+    räknaRecept()
+    recept()
+}
